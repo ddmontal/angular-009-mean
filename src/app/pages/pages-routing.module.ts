45c02982fd0990adebcf590655b9a6f03f630c23
David Montalvillo <david@Davids-MacBook-Pro.local>
2019-11-17 18:22:00 +0100

    feat: Use an observable to update page title

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -14,12 +14,12 @@ const routes: Routes = [
     path: '',
     component: PagesComponent,
     children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graficas1', component: Graficas1Component },
-      { path: 'account-settings', component: AccountSettingsComponent },
-      { path: 'promesas', component: PromesasComponent },
-      { path: 'rxjs', component: RxjsComponent },
+      { path: 'dashboard', component: DashboardComponent, data: { titulo: 'Dashboard' } },
+      { path: 'progress', component: ProgressComponent, data: { titulo: 'Progress' } },
+      { path: 'graficas1', component: Graficas1Component, data: { titulo: 'Gráficas' } },
+      { path: 'account-settings', component: AccountSettingsComponent, data: { titulo: 'Ajustes' } },
+      { path: 'promesas', component: PromesasComponent, data: { titulo: 'Promesas' } },
+      { path: 'rxjs', component: RxjsComponent, data: { titulo: 'Rxjs' } },
       { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
     ]
   }
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,10 +9,28 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class BreadcrumbsComponent implements OnInit {
+  titulo: string;
 
-  constructor() { }
+  constructor(private router: Router, private title: Title, private meta: Meta) {
+    this.getDataRoute().subscribe(event => {
+      this.titulo = event.titulo;
+      this.title.setTitle(this.titulo);
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.titulo
+      };
 
-  ngOnInit() {
+      this.meta.updateTag(metaTag);
+    });
   }
 
+  ngOnInit() {}
+
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter(event => event instanceof ActivationEnd),
+      filter((event: ActivationEnd) => event.snapshot.firstChild === null),
+      map((event: ActivationEnd) => event.snapshot.data)
+    );
+  }
 }
